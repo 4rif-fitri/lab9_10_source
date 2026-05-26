4357896685b227e878f0a73c439add850e1313c8
Arif <dopymonster@gmail.com>
2026-05-26 15:42:34 +0800

    update

--- components/footer.php
+++ components/footer.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../style/format.css">
+
 </head>
+
 <body>
     <footer>
         <h4 class="right">&copy; norazlin@utem.edu.my 2020 - <?php echo Date("Y"); ?> </h4>
     </footer>
 </body>
+
 </html>
